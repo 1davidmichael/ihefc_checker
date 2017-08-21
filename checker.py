1d73f95c3920e7b7a6c5fddcfc3c766aebfae93f
David Michael <david.michael@meredith.com>
2017-08-21 14:36:34 -0500

    WIP example of checking for links that give 200 response

--- checker.py
+++ checker.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
+import datetime
 import requests
+import boto3
+
+today = datetime.date.today()
+idx = (today.weekday() + 1) % 7
+last_sunday = (today - datetime.timedelta(idx))
 
 response = requests.get('http://www.ihefc.org/home/sermons.aspx')
 
-soup = BeautifulSoup(response.txt)
+soup = BeautifulSoup(response.text, "html.parser")
+
+recent_link = []
+for link in soup.findAll('a', attrs={'class': 'audioLink'}):
+    sermon_link = link['href']
+
+    if str(last_sunday) in sermon_link:
+        recent_link.append(sermon_link)
+
+if not recent_link:
+    print("Audio URL not present")
+
+test_link = requests.head(recent_link[0])
+if test_link.status_code != 200:
+    ses_response = ses.send_email(
+        Source = email_from,
+    )
